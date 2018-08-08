eefab76c8a2e031b22429ecc473fc28cebbd53b1
Wang Mengzhe <mengzhe.wang@intra.im>
2018-08-08 12:07:54 +0800

    debug

--- dpdk-latency/main.c
+++ dpdk-latency/main.c
@@ -185,6 +185,7 @@ inline static unsigned long long timestamp_nanosecs()
 {
     struct timespec timestamp;
     clock_gettime(CLOCK_MONOTONIC, &timestamp);
+    printf("%lu.%lu\n", timestamp.tv_sec, timestamp.tv_nsec);
     return (unsigned long long)(CLOCK_PRECISION * timestamp.tv_sec) + timestamp.tv_nsec;
 }
 
